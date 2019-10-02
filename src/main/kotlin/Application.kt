14c7808174b55149d3dec05671a329bf6232694e
Denis Krovyakov <denis.krovyakov@softomate.com>
2019-10-03 03:07:17 +0700

    created logic generated chain

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -1,15 +1,62 @@
 import gui.MainForm
-import javafx.application.Application
-import tornadofx.App
+import tornadofx.*
 
 class MainApp : App(MainForm::class)
 
-fun main(args: Array<String>) {
-    Application.launch(MainApp::class.java, *args)
+data class DataGrammatics(
+    val nonterminalSymbols : String,
+    val terminalSymbols: String,
+    val originSymbol: String,
+    val rulesSymbol: Map<Char,List<String>>
+)
+
+class GeneratedSequence(grammatics: DataGrammatics) {
+
+    private val nonterminals: String = grammatics.nonterminalSymbols
+    private val terminals: String = grammatics.terminalSymbols
+    private val selectedOriginSymbol = grammatics.originSymbol
+    private val rules = grammatics.rulesSymbol
+
+    val nonterminalsArray =  nonterminals.toCharArray()
+    val terminalsArray =  terminals.toCharArray()
+    private var listGeneratedString = listOf<String>()
+
+    private fun isNonterminal(char: Char): Boolean = nonterminalsArray.contains(char)
+    private fun isTerminal(char: Char): Boolean =  terminalsArray.contains(char)
+    private fun detectInfiniteRecursion(generatedSequence: String, maxLen: Int): Boolean {
+        val currentNonterminal  = generatedSequence.count { isNonterminal(it) }
+        if(currentNonterminal > maxLen) return true
+        return false
+    }
+
+    private fun generated(sequence: String, minLen: Int, maxLen: Int) {
+        val currentTerminals = sequence.count { isTerminal(it) }
+        if (currentTerminals > maxLen) return
+        val indexNonterminal = sequence.indexOfAny(nonterminalsArray)
+        if(indexNonterminal < 0) {
+            if(sequence.length >= minLen){
+                this.listGeneratedString = this.listGeneratedString + sequence
+            }
+            return
+        }
+        val nonteriman = sequence[indexNonterminal]
+        for ( rule in rules[nonteriman]!!){
+            val replaceString = sequence.replaceFirst(nonteriman.toString(),rule)
+            if(detectInfiniteRecursion(replaceString, maxLen)) continue
+            generated(replaceString, minLen, maxLen)
+        }
+    }
+
+    fun start (minLen: Int, maxLen: Int): List<String> {
+        val originSymbol = selectedOriginSymbol
+        generated(originSymbol, minLen, maxLen)
+        return this.listGeneratedString
+    }
+
 }
 
-//override fun start(stage: Stage) {
-//    stage.minHeight = 200.0
-//    stage.minWidth = 400.0
-//    super.start(stage)
-//}
+fun main(args: Array<String>) {
+    //Application.launch(MainApp::class.java, *args)
+
+}
+
